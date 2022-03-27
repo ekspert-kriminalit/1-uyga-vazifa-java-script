7c3ad068e6d30d080438fb1a9ec0d81db6ea18be
ekspert-kriminalist <101939877+ekspert-kriminalit@users.noreply.github.com>
2022-03-27 14:33:40 +0300

    add files

---
+++ script.js
@@ -0,0 +1,14 @@
+let Ism = prompt("Ismingiz nma ?")
+console.log("Ismingiz" + Ism);
+let Familiya = prompt("Familiyangiz nma ?")
+console.log("Familiyangiz" + Familiya);
+let yosh = prompt("Yoshingiz nechchida ?")
+console.log(yosh + "yoshdaman");
+let misol1 = prompt("5 + 5 nechchi boladi")
+console.log ("savol1ga javob" + (5 + 5) + "sizning javobingiz" + misol1 + "boldi");
+let misol2 = prompt("4 * 3 nechchi boladi")
+console.log ("savol2ga javob" + (4 * 3) + "sizning javobingiz" + misol2 + "boldi");
+let misol3 = prompt("15 - 4 nechchi boladi")
+console.log ("savol3ga javob" + (15 - 4) + "sizning javobingiz" + misol3 + "boldi");
+let misol4 = prompt("45 / 5 nechchi boladi")
+console.log ("savol4ga javob" + (45 / 5) + "sizning javobingiz" + misol4 + "boldi");
